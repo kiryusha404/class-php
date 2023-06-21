aa0ac52fbba94abf7238b818515987ad89d70669
Kirill <59690988+kiryusha404@users.noreply.github.com>
2023-06-21 13:55:04 +0400

    airport

--- class.php
+++ class.php
@@ -1,33 +1,75 @@
 <?php
 
-abstract class Plane{
+class Airport{ // Класс аэропорта
+
+    public $number_of_aircraft = 0; // количество самолётов в аэрпорту
+
+
+    public function  plane_is_ready(Airport $airport){ //  Функция сообщающая что самолёт готов взлетать
+        echo 'The plane is ready to take off';
+        echo '</br>';
+        $airport->plane_flew_away();
+    }
+    public function  plane_flew_away(){ //  Функция сообщающая что самолёт освободил место и улетел
+        $this->number_of_aircraft -=1;
+        echo 'The plane flew away';
+        echo '</br>';
+    }
+    public function take_plane(Airport $airport){ //  Функция принятия самолёта
+        echo 'The plane is accepted';
+        echo '</br>';
+        $airport->airplane_in_parking_lot();
+    }
+    public function  airplane_in_parking_lot(){ //  Функция сообщающая что самолёт на стоянку
+        $this->number_of_aircraft +=1;
+        echo 'The plane went to the parking lot';
+        echo '</br>';
+    }
+    public function  add_plane(){ //  Функция сообщающая что самолёт создан и добавлен на стоянку аэропорта
+        $this->number_of_aircraft += 1;
+    }
+    public function  get_status(){ //  Функция сообщающая сколько самолётов в аэрпорту
+        echo "Planes at the airport: ";
+        echo $this->number_of_aircraft;
+        echo '</br>';
+    }
+
+
+
+}
+
+abstract class Plane{ //абстракный класс самолёт
 
     public $name_plane;
     public $max_speed;
     public $height = 0;
 
-    public function __construct($name, $speed){ //Конструктор
+    public function __construct($name, $speed, Airport $airport){ //Конструктор
         $this->name_plane = $name;
         $this->max_speed = $speed;
+        $airport->add_plane();
     }
 
-    abstract protected function takeoff();
-    abstract protected function landing();
+    abstract protected function takeoff(Airport $airport);
+    abstract protected function landing(Airport $airport);
     abstract protected function get_status();
 }
 
-class MIG extends Plane
+class MIG extends Plane //класс определённого типа самолёта
 {
 
-    public function __construct($name, $speed){
-        parent::__construct($name, $speed);
+    public function __construct($name, $speed, Airport $airport){
+        parent::__construct($name, $speed, $airport);
     }
-    public function attack(){ //Фунция стрель самолета
+    public function attack(){ //Фунция стрельбы самолета
         echo $this->name_plane.': FIRE!!!';
         echo '</br>';
     }
 
-    public function takeoff(){ //Функция взлёта с проверкой летит ли самолёт
+    public function takeoff(Airport $airport){ //Функция взлёта с проверкой летит ли самолёт
+        if($this->height == 0){
+            $airport->plane_is_ready($airport);
+        }
         echo $this->name_plane;
         if($this->height == 0){
             $this->height += 1;
@@ -39,7 +81,10 @@ class MIG extends Plane
             echo '</br>';
         }
     }
-    public function landing(){ //Функция посадки с проверкой летит ли самолёт
+    public function landing(Airport $airport){ //Функция посадки с проверкой летит ли самолёт
+        if($this->height == 1){
+            $airport->take_plane($airport);
+        }
         echo $this->name_plane;
         if($this->height == 1){
             $this->height -= 1;
@@ -64,13 +109,16 @@ class MIG extends Plane
     }
 }
 
-class TY_154 extends Plane
+class TY_154 extends Plane //класс определённого типа самолёта
 {
-    public function __construct($name, $speed){
-        parent::__construct($name, $speed);
+    public function __construct($name, $speed, Airport $airport){
+        parent::__construct($name, $speed, $airport);
     }
 
-    public function takeoff(){ //Функция взлёта с проверкой летит ли самолёт
+    public function takeoff(Airport $airport){ //Функция взлёта с проверкой летит ли самолёт
+        if($this->height == 0){
+            $airport->plane_is_ready($airport);
+        }
         echo $this->name_plane;
         if($this->height == 0){
             $this->height += 1;
@@ -82,7 +130,10 @@ class TY_154 extends Plane
             echo '</br>';
         }
     }
-    public function landing(){ //Функция посадки с проверкой летит ли самолёт
+    public function landing(Airport $airport){ //Функция посадки с проверкой летит ли самолёт
+        if($this->height == 1){
+            $airport->take_plane($airport);
+        }
         echo $this->name_plane;
         if($this->height == 1){
             $this->height -= 1;
@@ -107,10 +158,17 @@ class TY_154 extends Plane
     }
 }
 
-$MIG1 = new MIG('bullet', 300);
-$MIG1->takeoff();
-$MIG1->takeoff();
+$airport = new Airport;
+
+$TY_154 = new TY_154('corn', 250, $airport);//Обьект класса ТУ
+$MIG2 = new MIG('blood', 330, $airport); // обьект класса МИГ
+$MIG1 = new MIG('bullet', 300, $airport); // обьект класса МИГ
+$MIG1->takeoff($airport);
+$MIG1->takeoff($airport);
 $MIG1->attack();
-$MIG1->landing();
+$MIG1->landing($airport);
 $MIG1->get_status();
+
+
+$airport->get_status();
 ?>
